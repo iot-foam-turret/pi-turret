64efa71b454cd445b13ff98c016ac2f0f251f203
Corwin Marsh <corwinm@users.noreply.github.com>
2019-08-28 22:45:17 -0700

    Cleanup

--- pi_turret/__main__.py
+++ pi_turret/__main__.py
@@ -1,4 +1,5 @@
 """This will be the main file that runs the automated turret"""
-print("Hello from Pi Turret")
+from pi_turret.test_scripts.keyboard_control import main
 
-import pi_turret.test_scripts.keyboard_control
+print("Hello from Pi Turret")
+main()
--- pi_turret/test_scripts/keyboard_control.py
+++ pi_turret/test_scripts/keyboard_control.py
@@ -4,41 +4,47 @@
 import curses
 from pi_turret.turret.turret import Turret
 
-TURRET = Turret()
+def main():
+    """Main script to control the turret with the keyboard
+    """
+    turret = Turret()
 
-# get the curses screen window
-SCREEN = curses.initscr()
+    # get the curses screen window
+    screen = curses.initscr()
 
-# turn off input echoing
-curses.noecho()
+    # turn off input echoing
+    curses.noecho()
 
-# respond to keys immediately (don't wait for enter)
-curses.cbreak()
+    # respond to keys immediately (don't wait for enter)
+    curses.cbreak()
 
-# map arrow keys to special values
-SCREEN.keypad(True)
+    # map arrow keys to special values
+    screen.keypad(True)
 
-try:
-    while True:
-        CHAR = SCREEN.getch()
-        if CHAR == ord('q'):
-            break
-        elif CHAR == curses.KEY_RIGHT:
-            TURRET.move_right()
-            # print doesn't work with curses, use addstr instead
-            SCREEN.addstr(0, 0, 'right')
-        elif CHAR == curses.KEY_LEFT:
-            TURRET.move_left()
-            SCREEN.addstr(0, 0, 'left ')
-        elif CHAR == curses.KEY_UP:
-            TURRET.move_up()
-            SCREEN.addstr(0, 0, 'up   ')
-        elif CHAR == curses.KEY_DOWN:
-            TURRET.move_down()
-            SCREEN.addstr(0, 0, 'down ')
-finally:
-    # shut down cleanly
-    curses.nocbreak()
-    SCREEN.keypad(0)
-    curses.echo()
-    curses.endwin()
+    try:
+        while True:
+            char = screen.getch()
+            if char == ord('q'):
+                break
+            elif char == curses.KEY_RIGHT:
+                turret.move_right()
+                # print doesn't work with curses, use addstr instead
+                screen.addstr(0, 0, 'right')
+            elif char == curses.KEY_LEFT:
+                turret.move_left()
+                screen.addstr(0, 0, 'left ')
+            elif char == curses.KEY_UP:
+                turret.move_up()
+                screen.addstr(0, 0, 'up   ')
+            elif char == curses.KEY_DOWN:
+                turret.move_down()
+                screen.addstr(0, 0, 'down ')
+    finally:
+        # shut down cleanly
+        curses.nocbreak()
+        screen.keypad(0)
+        curses.echo()
+        curses.endwin()
+
+if __name__ == "__main__":
+    main()
