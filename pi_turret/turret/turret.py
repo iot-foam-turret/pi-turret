b63dcc1419ad5a207372a7a353778521f896b783
Corwin Marsh <corwinm@users.noreply.github.com>
2019-09-09 09:04:27 -0700

    Add max yaw and pitch

--- pi_turret/turret/turret.py
+++ pi_turret/turret/turret.py
@@ -1,4 +1,5 @@
-"""Main Blaster Turret
+"""
+Main Blaster Turret
 """
 import time
 from pi_turret.blaster.hyperfire import Hyperfire
@@ -7,8 +8,12 @@ from pi_turret.stepper_motor.stepper_slot import StepperMotorSlot
 from pi_turret.sensor.button import yaw_button, pitch_button
 
 class Turret:
-    """Turret that can move left/right and up/down
     """
+    Turret that can move left/right and up/down
+    """
+
+    yaw_max = 90
+    pitch_max = 20
 
     def __init__(self):
         self.blaster = Hyperfire()
@@ -18,7 +23,8 @@ class Turret:
         self.yaw = None
 
     def calibrate(self):
-        """Calibrate the position of the stepper motors
+        """
+        Calibrate the position of the stepper motors
         """
         yaw_sensor = yaw_button()
         while not yaw_sensor.is_pressed():
@@ -36,29 +42,49 @@ class Turret:
         self.yaw = 0
 
     def move(self, pitch: float, yaw: float):
+        """
+        Move the turret to the given pitch and yaw
+        """
         move_pitch = pitch - self.pitch
         move_yaw = yaw - self.yaw
 
         while move_pitch is not 0 or move_yaw is not 0:
             if move_pitch > 0:
                 self.pitch_motor.one_step_backwards()
+                self.pitch += STEP_DEGREES
                 move_pitch -= STEP_DEGREES
             elif move_pitch < 0:
                 self.pitch_motor.one_step_forward()
+                self.pitch -= STEP_DEGREES
                 move_pitch += STEP_DEGREES
-            if abs(move_pitch) < (STEP_DEGREES / 2):
+            if abs(move_pitch) < (STEP_DEGREES / 2) or self.pitch_stop():
                 move_pitch = 0
 
             if move_yaw > 0:
                 self.yaw_motor.one_step_backwards()
+                self.yaw += STEP_DEGREES
                 move_yaw -= STEP_DEGREES
             elif move_pitch < 0:
                 self.yaw_motor.one_step_forward()
+                self.yaw -= STEP_DEGREES
                 move_yaw += STEP_DEGREES
-            if abs(move_yaw) < (STEP_DEGREES / 2):
+            if abs(move_yaw) < (STEP_DEGREES / 2) or self.yaw_stop():
                 move_yaw = 0
 
             time.sleep(0.02)
+
+    def yaw_stop(self):
+        """
+        Return true if the yaw position is beyond the functional bound.
+        """
+        return abs(self.yaw) > self.yaw_max
+
+
+    def pitch_stop(self):
+        """
+        Return true if the pitch position is beyond the functional bound.
+        """
+        return abs(self.pitch) > self.pitch_max
 
 
     def move_up(self):
